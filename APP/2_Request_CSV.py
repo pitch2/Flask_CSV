df849b00c981617301477deb07ac1d5813881f53
pitch <95540893+pitch2@users.noreply.github.com>
2025-07-14 22:50:14 +0200

    Add files via upload

--- APP/2_Request_CSV.py
+++ APP/2_Request_CSV.py
@@ -13,17 +13,28 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def affichage_pandas_base():
-    return render_template_string(f"For request : /request/<strong>name_of_file</strong>/<strong>column</strong>/<strong>number</strong>")
+    return render_template("home.html")
+
+@app.route("/request/<file>")
+def request_head_default(file):
+    return render_template_string('{{ data|safe }}', data=pd.read_csv(f"./CSV_files/{file}.csv").head(1).to_html())
+
+@app.route("/request/<file>/<int:number_column>")
+def request_head_number(file, number_column):
+    return render_template_string('{{ data|safe }}', data=pd.read_csv(f"./CSV_files/{file}.csv").head(int(number_column)).to_html())
 
 @app.route("/request/<file>/<column>/<int:number_column>")
 def request(file, column, number_column):
     return render_template_string(((pd.read_csv(fr".\CSV_files\{file}.csv")[[column]]).head(int(number_column))).to_html())
 
-
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in allowed_extensions
 
+@app.route("/listing")
+def liste_CSV():
+    liste = (' ; '.join(os.listdir("CSV_files")))
+    return render_template("liste_CSV.html", liste=liste)
 
 @app.route("/upload_file", methods=['GET', 'POST'])
 def upload_file():
