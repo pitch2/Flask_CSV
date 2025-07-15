94b4d4a13d6e5e649b0ec137b90caeb00a69c530
pitch <95540893+pitch2@users.noreply.github.com>
2025-07-15 22:12:03 +0200

    Add files via upload

--- APP/2_Request_CSV.py
+++ APP/2_Request_CSV.py
@@ -24,7 +24,7 @@ def request_head_number(file, number_column):
     return render_template_string('{{ data|safe }}', data=pd.read_csv(f"./CSV_files/{file}.csv").head(int(number_column)).to_html())
 
 @app.route("/request/<file>/<column>/<int:number_column>")
-def request(file, column, number_column):
+def request_attribut(file, column, number_column):
     return render_template_string(((pd.read_csv(fr".\CSV_files\{file}.csv")[[column]]).head(int(number_column))).to_html())
 
 def allowed_file(filename):
@@ -49,7 +49,8 @@ def upload_file():
         if file and allowed_file(file.filename): #Si ok ->
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return f"Upload OK"
+            print(filename, file)
+            return render_template_string('{{ data|safe }}', data=pd.read_csv(f"./CSV_files/{filename.split(".")[0]}.csv").head(1).to_html())
     return '''
     <!doctype html>
     <title>Upload new File</title>
